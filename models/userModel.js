aac823d08ccb4578bbcc244f9b3269fada326429
vanragas <sagar.samundar@gmail.com>
2024-09-27 12:47:37 +0530

    signup & login functions

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -18,12 +19,37 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a passowrd.'],
     minlength: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password.'],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message:
+        "Incorrect password. Password does't match. Please type correct password.",
+    },
   },
 });
+
+//Password encryption document middleware
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
+});
+
+//Instance method
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword,
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
